a0c554c4f8e11fe39951d0d74e0fb7c9e9c77772
Jesse Snyder <jessetsnyder@gmail.com>
2023-06-21 15:10:05 -0600

    astria styling

--- web/src/Faucet.svelte
+++ web/src/Faucet.svelte
@@ -71,86 +71,98 @@
 </script>
 
 <main>
-  <section class="hero is-info is-fullheight">
-    <div class="hero-head">
-      <nav class="navbar">
-        <div class="container">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="../..">
-              <span class="icon">
-                <i class="fa fa-bath" />
-              </span>
-              <span><b>ETH Faucet</b></span>
-            </a>
-          </div>
-          <div id="navbarMenu" class="navbar-menu">
-            <div class="navbar-end">
-              <span class="navbar-item">
-                <a
-                  class="button is-white is-outlined"
-                  href="https://github.com/chainflag/eth-faucet"
-                >
-                  <span class="icon">
-                    <i class="fa fa-github" />
-                  </span>
-                  <span>View Source</span>
-                </a>
-              </span>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <section class="hero is-info is-fullheight">
+        <div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
 
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="column is-6 is-offset-3">
-          <h1 class="title">
-            Receive {faucetInfo.payout} ETH per request
-          </h1>
-          <h2 class="subtitle">
-            Serving from {faucetInfo.account}
-          </h2>
-          <div class="box">
-            <div class="field is-grouped">
-              <p class="control is-expanded">
-                <input
-                  bind:value={input}
-                  class="input is-rounded"
-                  type="text"
-                  placeholder="Enter your address or ENS name"
-                />
-              </p>
-              <p class="control">
-                <button
-                  on:click={handleRequest}
-                  class="button is-primary is-rounded"
-                >
-                  Request
-                </button>
-              </p>
-            </div>
-          </div>
+                    <div class="navbar-brand">
+                        <a class="navbar-item is-white"
+                           href="https://astria.org"
+                           target="_blank"
+                        >
+                            <span><b>Astria</b></span>
+                        </a>
+                    </div>
+
+                    <div id="navbarMenu" class="navbar-menu">
+                        <div class="navbar-end">
+                          <span class="navbar-item">
+                            <a class="button is-white is-outlined"
+                               href="https://github.com/astriaorg/eth-faucet"
+                               target="_blank"
+                            >
+                              <span class="icon">
+                                <i class="fa fa-github"></i>
+                              </span>
+                              <span>View Source</span>
+                            </a>
+                          </span>
+                        </div>
+                    </div>
+
+                </div>
+            </nav>
         </div>
-      </div>
-    </div>
-  </section>
+
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-6 is-offset-3">
+                    <h1 class="title">
+                        Receive {faucetInfo.payout} RIA per request
+                    </h1>
+                    <h2 class="subtitle is-6">
+                        Serving from {faucetInfo.account}
+                    </h2>
+                    <div class="box">
+                        <div class="field is-grouped">
+                            <p class="control is-expanded">
+                                <input
+                                        bind:value={input}
+                                        class="input is-rounded"
+                                        type="text"
+                                        placeholder="Enter your address or ENS name"
+                                />
+                            </p>
+                            <p class="control">
+                                <button on:click={handleRequest}
+                                        class="button is-white is-outlined"
+                                >
+                                    Request
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <style>
-  .hero.is-info {
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-      url('/background.jpg') no-repeat center center fixed;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-  }
-  .hero .subtitle {
-    padding: 3rem 0;
-    line-height: 1.5;
-  }
-  .box {
-    border-radius: 19px;
-  }
+    .hero.is-info {
+        background: black url('/hero-blocks.webp') no-repeat fixed center center;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+
+    .hero.is-info a.navbar-item:hover {
+        background-color: transparent;
+    }
+
+    .hero .subtitle {
+        padding: 3rem 0;
+        line-height: 1.5;
+    }
+
+    .box {
+        border-radius: 0;
+        background: transparent;
+    }
+
+    .button {
+        border-radius: 0;
+    }
 </style>
